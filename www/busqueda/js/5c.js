57955832d10e04f2600ae7864e951526017d39ee
Jorge Andres Moreno Jaimes <jamper91@hotmail.com>
2014-04-11 01:40:18 -0500

    Cambios:
    Se agrega un mapa que sera visible en caso de que el medio de transporte sea automovil

--- www/busqueda/js/5c.js
+++ www/busqueda/js/5c.js
@@ -14,6 +14,8 @@ $(document).ready(
 
 function getInformacion(idMedioTransporte, idCentroComercial)
 {
+    log("5c","getInformacion", "idMedioTransporte: "+idMedioTransporte);
+    log("5c","getInformacion", "idCentroComercial: "+idCentroComercial);
     var url="";
     var datos={
         idMedioTransporte:idMedioTransporte,
@@ -27,12 +29,39 @@ function getInformacion(idMedioTransporte, idCentroComercial)
             var nombre,informacion;
             $("#nombre").text(nombre);
             $("#informacion").val(informacion);
+            
         });
     }else
     {
-         var nombre="Transporte Publico",informacion="Cojase un transmilleno y se baja donde vea que esta perdido";
+
+            var nombre="Transporte Publico",informacion="Cojase un transmilleno y se baja donde vea que esta perdido";
             $("#nombre").text(nombre);
             $("#informacion").val(informacion);
+            activarMapa();
+         
     }
     
+}
+
+function activarMapa()
+{
+    /*$("#map_canvas").attr("display","block");*/
+    var mapCanvas = document.getElementById('map_canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(4.6753515,-74.0479714),
+      zoom: 18,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(4.6753515,-74.0479714),
+      map: map,
+      icon: iconBase + 'schools_maps.png'
+    });
+
+
+  
+
 }
